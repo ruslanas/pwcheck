9b0c3da747672a435a0ad1511d825e0f617eee05
Ruslanas Balčiūnas <ruslanas.com@gmail.com>
2017-11-25 17:08:33 +0200

    Init

---
+++ README.md
@@ -0,0 +1,23 @@
+Pwned password checker
+======================
+
+Build
+-----
+
+`cargo build --release`
+
+Usage example
+-------------
+
+`pwcheck pwned-passwords-1.0.txt p@55word`
+
+Output
+------
+
+```
+306259512 password hashes in file.
+SHA1 3558288C5E578A05536EC28C4C1613AE865FE75C
+Found at line: 63563447
+```
+
+[Pwned Passwords list](https://haveibeenpwned.com/Passwords)
---
+++ src/main.rs
@@ -0,0 +1,104 @@
+/*
+Copyright © 2017 Ruslanas Balčiūnas
+Email: ruslanas.com@gmail.com
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+extern crate crypto;
+
+use std::io;
+use crypto::digest::Digest;
+use crypto::sha1::Sha1;
+use std::fs;
+use std::fs::File;
+use std::io::BufReader;
+use std::io::prelude::*;
+use std::env;
+use std::process;
+use std::cmp::Ordering;
+
+fn main() {
+
+    let mut args = env::args();
+
+    if args.len() < 3 {
+        println!("Usage: pwcheck <file> <pwd>");
+        process::exit(0);
+    }
+
+    // consumes preceding elements
+    let fname = args.nth(1)
+        .expect("Fail reading filename");
+
+    let mut hasher = Sha1::new();
+    hasher.input_str(
+        args.nth(0)
+            .expect("Fail reading password")
+            .as_str()
+    );
+    
+    let hash = hasher.result_str().to_uppercase();
+
+    let metadata = fs::metadata(fname.clone())
+        .expect("Failed to read file metadata");
+
+    let chars = metadata.len();
+    let lines = chars / 42;
+    
+    println!("{} password hashes in file.", lines);
+    println!("SHA1 {}", hash);
+
+    let file = File::open(fname)
+        .expect("Failed to open file for reading");
+    
+    let mut reader = BufReader::new(file);
+
+    let mut _start = 0;
+    let mut _end = lines - 1;
+    let mut new_pos = _end;
+    let mut old_pos = _start;
+    let mut found = false;
+
+    while !found && (new_pos != old_pos) {
+        
+        old_pos = new_pos;
+        new_pos = (_end + _start) / 2;
+
+        reader.seek(io::SeekFrom::Start(new_pos * 42)).expect("Seek fail");
+
+        let mut line = String::new();
+        reader.read_line(&mut line)
+            .expect("Failed to read line");
+
+        line = line.trim().to_string();
+        let cmp = hash.cmp(&line);
+
+        match cmp {
+            Ordering::Greater => _start = new_pos,
+            Ordering::Less => _end = new_pos,
+            Ordering::Equal => {
+                found = true;
+            }
+        }
+
+        // println!("{}\t{}\t{:?}", new_pos, line.trim(), cmp);
+
+    }
+
+    if found {
+        println!("Found at line: {}", new_pos);
+    } else {
+        println!("Not found!");
+    }
+}
